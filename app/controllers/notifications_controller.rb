a327807875fc794742f79b040fbbb5f7237dbed1
EC2 Default User <ec2-user@ip-172-31-33-188.us-east-2.compute.internal>
2022-04-18 03:20:29 +0000

    view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,12 @@
 class NotificationsController < ApplicationController
   def index
    　#通知データを取得
-    @notifications = current_user.passive_notifications.order(created_at: :desc)
+    #@notifications = current_user.passive_notifications
+    @notifications = Notification.where(visited_id: 3)
 	#通知一覧に遷移したら、既読済みにする
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
-     end
+    #@notifications.where(checked: false).each do |notification|
+     # notification.update_attributes(checked: true)
+     #end
   end
 
 end
