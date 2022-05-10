79ca7c17e8163729c2cc66b896234f94c5631cb0
EC2 Default User <ec2-user@ip-172-31-33-188.us-east-2.compute.internal>
2022-05-10 08:34:42 +0000

    view途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,6 @@ class ApplicationController < ActionController::Base
       def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: %i[last_name first_name is_gender phone_number])
       end
-  
+      
+
 end
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,17 @@
 module NotificationsHelper
+    
+   def notification_form(notification)
+      @visiter = notification.visiter
+      @impression = nil
+      your_item = link_to 'あなたのレシピ', recipe_path(notification), style:"font-weight: bold;"
+      @visiter_comment = notification.impression_id
+      #notification.actionがfollowかlikeかcommentか
+      case notification.action
+        when "favorite" then
+          tag.a(notification.visiter.nickname, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたのレシピ', href:recipe_path(notification.recipe_id), style:"font-weight: bold;")+"をお気に入り登録しました"
+        when "impression" then
+            @comment = Comment.find_by(id: @visiter_comment)&.content
+            tag.a(@visiter.nickname, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:recipe_path(notification.recipe_id), style:"font-weight: bold;")+"にコメントしました"
+      end
+　  end
 end
